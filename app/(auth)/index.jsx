56d8aa70857b4a2e32d0b38cef22dd2b8e759fed
Sagar Adhikari.blade.php <164398516+Sagarhoraa@users.noreply.github.com>
2025-05-24 22:22:28 +0545

    Update index.jsx

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,18 +1,26 @@
-import { View, Text, Image, TextInput, Touchable, TouchableOpacity, ActivityIndicator,StyleSheet, KeyboardAvoidingView, Platform} from 'react-native'
+import { View, Text, Image, TextInput, Touchable, TouchableOpacity, ActivityIndicator,StyleSheet, KeyboardAvoidingView, Platform, Alert} from 'react-native'
 import styles from "../../assets/styles/login.styles";
 import { useState } from 'react';
 import { Ionicons } from "@expo/vector-icons";
 import COLORS from '../../constants/colors';
 import {Link } from "expo-router";
 
+import {useAuthStore} from "../../store/authStore";
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const[showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const{ isLoading, Login} = useAuthStore();
 
-  const handleLogin = () =>{};
+
+  const handleLogin = async() =>{
+    const result= await Login(email, password0);
+
+    if(!result.success) Alert.alert("Error", result.error);
+
+  };
   return (
     <KeyboardAvoidingView 
     style={{ flex: 1}}
